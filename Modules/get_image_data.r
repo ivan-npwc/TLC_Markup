fdb3bc43f04cc91cfcda32dab2520721f001fba6
Ivan <ivan@npwc.us>
2025-09-09 16:39:03 +1000

    20250909

--- Modules/get_image_data.r
+++ Modules/get_image_data.r
@@ -1,43 +1,250 @@
-########################################################################
-get_image_data <<- function(image_directory) {
- library(magick)
- library(tools)
-# image_directory="E:\\Image uniq sites folders"
-  jpeg_files <- list.files(image_directory, full.names = TRUE, ignore.case = TRUE, recursive=T,  pattern = "\\.(jpg|jpeg|png|gif|bmp|tiff|tif)$")
-  results <- NULL
-  for (i in 1:length(jpeg_files)) {
+get_image_data_parallel_unix <- function(image_directory) {
+  library(EBImage)
+  library(tools)
+  library(parallel)
+  library(future.apply)
+  library(pbapply)
   
-    image_path=jpeg_files[i]
-   # img <- image_read(image_path)
-
-  #   img_info <- image_info(img)
-     width <- ""#img_info$width
-     height <- ""#img_info$height
+  # Находим все JPG файлы
+  jpeg_files <- list.files(image_directory, full.names = TRUE, 
+                          ignore.case = TRUE, recursive = TRUE, 
+                          pattern = "\\.(jpg|JPG|jpeg|JPEG)$")
+  
+  if (length(jpeg_files) == 0) {
+    warning("No JPG files found in the directory")
+    return(data.frame())
+  }
+  
+  # Функция для обработки одного изображения
+  process_single_image <- function(image_path) {
+    tryCatch({
+      # Пытаемся прочитать изображение
+      img <- readImage(image_path)
+      
+      # Получаем размеры
+      width <- dim(img)[2]  # исправлено: width обычно второй dimension
+      height <- dim(img)[1] # height обычно первый
+      
+      # Извлекаем информацию из пути
       file_name <- basename(image_path)
-	  dtime = substr(file_name,1,16)
-	  ext = file_ext(file_name)
-	  ext1=paste0(".",ext)
-	  poly0 = gsub(dtime,"",file_name)
-	  poly = gsub(ext1,"",poly0)
-
-      site0=gsub(file_name,"",image_path)
-      date=basename(site0)
-      site1=gsub(date,"",site0)
-      site2=basename(site1)
-	  site3=strsplit(site2,"_")[[1]][2]
-	  
-      pre=data.frame(
-	    image_path = image_path,
-        site = paste0("site_",site3),
-        poly = paste0("poly_",poly),
-        dimensions = paste0(height," ", width),
-		stringsAsFactors = FALSE
-        
+      dtime <- substr(file_name, 1, 16)
+      ext <- file_ext(file_name)
+      ext1 <- paste0(".", ext)
+      poly0 <- gsub(dtime, "", file_name)
+      poly <- gsub(ext1, "", poly0)
+      
+      site0 <- gsub(file_name, "", image_path)
+      date <- basename(site0)
+      site1 <- gsub(date, "", site0)
+      site2 <- basename(site1)
+      site3 <- ifelse(length(strsplit(site2, "_")[[1]]) >= 2, 
+                     strsplit(site2, "_")[[1]][2], 
+                     "unknown")
+      
+      # Возвращаем результат
+      list(
+        image_path = image_path,
+        site = paste0("site_", site3),
+        poly = paste0("poly_", poly),
+        width = width,
+        height = height,
+        dimensions = paste0(width, "x", height),
+        status = "success"
       )
-	results = rbind(pre,results)
-	  
-    }
-
-  return(results)
+      
+    }, error = function(e) {
+      # В случае ошибки возвращаем запись с ошибкой
+      list(
+        image_path = image_path,
+        site = NA_character_,
+        poly = NA_character_,
+        width = "error",
+        height = "error",
+        dimensions = "error",
+        status = "error"
+      )
+    })
+  }
+  
+  # Вариант 1: Использование mclapply (только для Unix)
+  if (.Platform$OS.type == "unix") {
+    cat("Using mclapply for parallel processing on Unix...\n")
+    
+    results_list <- mclapply(jpeg_files, function(img_path) {
+      process_single_image(img_path)
+    }, mc.cores = detectCores(), mc.preschedule = TRUE)
+    
+  } else {
+    # Для Windows используем обычный lapply
+    warning("Running on Windows, using single-core processing")
+    results_list <- lapply(jpeg_files, process_single_image)
+  }
+  
+  # Преобразуем список в data.frame
+  results_df <- do.call(rbind, lapply(results_list, as.data.frame, stringsAsFactors = FALSE))
+  
+  return(results_df)
 }
-###################################################################
+
+# Версия с прогресс-баром для Unix
+get_image_data_parallel_unix_pb <- function(image_directory) {
+  library(EBImage)
+  library(tools)
+  library(parallel)
+  library(pbapply)
+  
+  jpeg_files <- list.files(image_directory, full.names = TRUE, 
+                          ignore.case = TRUE, recursive = TRUE, 
+                          pattern = "\\.(jpg|JPG|jpeg|JPEG)$")
+  
+  if (length(jpeg_files) == 0) {
+    warning("No JPG files found in the directory")
+    return(data.frame())
+  }
+  
+  process_single_image <- function(image_path) {
+    tryCatch({
+      img <- readImage(image_path)
+      width <- dim(img)[2]
+      height <- dim(img)[1]
+      
+      file_name <- basename(image_path)
+      dtime <- substr(file_name, 1, 16)
+      ext <- file_ext(file_name)
+      ext1 <- paste0(".", ext)
+      poly0 <- gsub(dtime, "", file_name)
+      poly <- gsub(ext1, "", poly0)
+      
+      site0 <- gsub(file_name, "", image_path)
+      date <- basename(site0)
+      site1 <- gsub(date, "", site0)
+      site2 <- basename(site1)
+      site3 <- ifelse(length(strsplit(site2, "_")[[1]]) >= 2, 
+                     strsplit(site2, "_")[[1]][2], 
+                     "unknown")
+      
+      list(
+        image_path = image_path,
+        site = paste0("site_", site3),
+        poly = paste0("poly_", poly),
+        width = width,
+        height = height,
+        dimensions = paste0(width, "x", height),
+        status = "success"
+      )
+      
+    }, error = function(e) {
+      list(
+        image_path = image_path,
+        site = NA_character_,
+        poly = NA_character_,
+        width = "error",
+        height = "error",
+        dimensions = "error",
+        status = "error"
+      )
+    })
+  }
+  
+  # Используем pbmclapply для прогресс-бара + параллелизма на Unix
+  if (.Platform$OS.type == "unix") {
+    cat("Using pbmclapply with progress bar on Unix...\n")
+    
+    results_list <- pbmclapply(jpeg_files, process_single_image, 
+                              mc.cores = detectCores(),
+                              ignore.interactive = FALSE)
+  } else {
+    warning("Running on Windows, using single-core with progress bar")
+    results_list <- pblapply(jpeg_files, process_single_image)
+  }
+  
+  results_df <- do.call(rbind, lapply(results_list, as.data.frame, stringsAsFactors = FALSE))
+  return(results_df)
+}
+
+# Версия с будущими вычислениями (современный подход)
+get_image_data_future <- function(image_directory) {
+  library(EBImage)
+  library(tools)
+  library(future)
+  library(future.apply)
+  library(progressr)
+  
+  jpeg_files <- list.files(image_directory, full.names = TRUE, 
+                          ignore.case = TRUE, recursive = TRUE, 
+                          pattern = "\\.(jpg|JPG|jpeg|JPEG)$")
+  
+  # Настраиваем параллельное выполнение
+  if (.Platform$OS.type == "unix") {
+    plan(multicore, workers = availableCores() - 1)
+  } else {
+    plan(multisession, workers = availableCores() - 1)
+  }
+  
+  process_single_image <- function(image_path) {
+    tryCatch({
+      img <- readImage(image_path)
+      width <- dim(img)[2]
+      height <- dim(img)[1]
+      
+      file_name <- basename(image_path)
+      dtime <- substr(file_name, 1, 16)
+      ext <- file_ext(file_name)
+      ext1 <- paste0(".", ext)
+      poly0 <- gsub(dtime, "", file_name)
+      poly <- gsub(ext1, "", poly0)
+      
+      site0 <- gsub(file_name, "", image_path)
+      date <- basename(site0)
+      site1 <- gsub(date, "", site0)
+      site2 <- basename(site1)
+      site3 <- ifelse(length(strsplit(site2, "_")[[1]]) >= 2, 
+                     strsplit(site2, "_")[[1]][2], 
+                     "unknown")
+      
+      data.frame(
+        image_path = image_path,
+        site = paste0("site_", site3),
+        poly = paste0("poly_", poly),
+        width = width,
+        height = height,
+        dimensions = paste0(width, "x", height),
+        status = "success",
+        stringsAsFactors = FALSE
+      )
+      
+    }, error = function(e) {
+      data.frame(
+        image_path = image_path,
+        site = NA_character_,
+        poly = NA_character_,
+        width = "error",
+        height = "error",
+        dimensions = "error",
+        status = "error",
+        stringsAsFactors = FALSE
+      )
+    })
+  }
+  
+  # Параллельная обработка с прогресс-баром
+  with_progress({
+    p <- progressor(along = jpeg_files)
+    
+    results_list <- future_lapply(jpeg_files, function(img_path) {
+      result <- process_single_image(img_path)
+      p()
+      return(result)
+    }, future.seed = TRUE)
+  })
+  
+  results_df <- do.call(rbind, results_list)
+  return(results_df)
+}
+
+# Пример использования:
+ result1 <- get_image_data_parallel_unix("/media/ivan/2023_ HD2/SSL_DB")
+#result2 <- get_image_data_parallel_unix_pb("/media/ivan/2023_ HD2/SSL_DB/2024_683_Map/20241005") # с прогресс-баром
+ #result3 <- get_image_data_future("/media/ivan/2023_ HD2/SSL_DB") # современный подход
+ 
+ write.csv(result1,"image_data.csv",row.names=F)
--- Modules/tiling.r
+++ Modules/tiling.r
@@ -1,38 +1,144 @@
 
-
-  tiling=function(outdir,imgdata,RDSdata){
+#source("/home/ivan/GIT_HUB/TLC_Markup/Modules/tiling.r")
   library(EBImage)
   library(sp)
   library(sf)
+  library(tools)
+  library(doMC) 
+  ###########################
+   num_cores <- detectCores()  - 4
+   registerDoMC(cores = num_cores)
+  ##########################
+  indir= "/media/ivan/2023_ HD2/SSL_DB"
+  outdir =  "/media/ivan/USATOV_2024/SSL_DB_Tiles"
+  RDSpth = "/home/ivan/GIT_HUB/TLC_Markup/image_tiles.rds"
+  imgsdtpth = "/home/ivan/image_data.csv"
+  control_tmp_pth="control_tmp.csv"
+ ######################################### 
+  RDSdata = readRDS(RDSpth)
+  imgs_dt=read.csv(imgsdtpth)
+  if (dir.exists(indir)==F) {stop("NO IN DIR FOUND")}
+  if (dir.exists(outdir)==F) {stop("NO OUT DIR FOUND")}
+  if (file.exists(control_tmp_pth)==T){control_tmp=read.csv(control_tmp_pth)} else {control_tmp=data.frame(site="",day="",cam="")}
+  ##########################
+  head(imgs_dt)
+  imgs_dt=imgs_dt[imgs_dt$status == "success",]
+ ###########################################chesk 
+  source("/home/ivan/GIT_HUB/TLC_Markup/Modules/RDStoTable.r")
+	 RDStbl =RDStoTable(RDSpth)
+	 RDStbl$sitepoly=paste0("site_",RDStbl$site,"#","poly_", RDStbl$poly)
+	 imgs_dt$sitepoly=paste0(imgs_dt$site,"#",imgs_dt$poly) 
+	 err=imgs_dt[!imgs_dt$sitepoly %in% RDStbl$sitepoly,]
+	 length(err$image_path)
+     uniqerr=unique(err$sitepoly)
+ if(length(uniqerr>0)){
+ errDir =paste0(outdir,"/NEDD_MARKUP"); dir.create(errDir)
+   for (i in 1:length(uniqerr)){
+   cam=uniqerr[i]
+   imgsNeedMarkUp =imgs_dt[imgs_dt$sitepoly == cam,]
+   index=sample(1 : length(imgsNeedMarkUp$sitepoly))[1:30]
+   imgspth =imgsNeedMarkUp[index,]
+      for(y in 1:length(imgspth)){
+	  row1 = imgspth[y,]
+	  site= gsub("site_","",row1$site)
+	  day=row1$day
+	  year= substr(basename(row1$image_path),1,4)
+	  Mapyearfold =paste0(errDir,"/",year,"_",site,"_Map");dir.create(Mapyearfold,showWarnings = F)
+	  dayfold= paste0(Mapyearfold,"/",day);dir.create(dayfold,showWarnings = F)
+    file.copy(row1$image_path,dayfold)
+ }
+ }
+ paste0("PLEASE  REVIEW AND  MAKE MARCKUP FOR   IMAGES IN FOLDER  " ,errDir  )
+ stop(paste0("No markUp found for ",   data.frame(uniqerr)))
+ } 
+#######################################################################
+ if (file.exists(imgs_dt$image_path[1])==F){
+    for (i in 1:length(imgs_dt$image_path)){  # for case if SSL_db pth changed
+	pth=imgs_dt$image_path[i]
+	bspth=strsplit(pth,"SSL_DB")
+	newpth=paste0(indir,bspth[[1]][2])
+	imgs_dt$image_path[i]=newpth
+	}}
+################################################
+###############################################################################################
+imgs_dt$day=substr(basename(imgs_dt$image_path),1,8)
+##############	
+uniqsites =unique(imgs_dt$site)	
+uniqcam =unique(imgs_dt$poly)
+uniqday =unique(imgs_dt$day)
 
+ for (sts in 1: 2){ #length(uniqsites)
+    site = uniqsites[sts]
+	print(site)
+	site_no_site =gsub("site_","",site)
+	#site_site =paste0("site_",site)
+	site_data = imgs_dt[imgs_dt$site==site,]
+	site_days =unique(site_data$day)
+	  for (ds in 1:length(site_days)){
+	  day1 =site_days[ds]
+	  print(day1)
+	  site_day_data = site_data[site_data$day==day1,]
+	  site_days_cam =unique(site_day_data$poly)
+	      for (pl in 1:length(site_days_cam)){
+		  
+		  poly1 = site_days_cam[pl]
+		  print(poly1)
+		  cam = gsub("poly","",poly1)
+		  cam=paste0(cam,".JPG")
+		  
+	#	   if (site  %in%  control_tmp$site &
+#		      poly1 %in% control_tmp$cam &
+#			  day1 %in% control_tmp$day
+#			  ) next
+		  
+		  
+	
+		    site_days_cam_data =site_day_data[site_day_data$poly == poly1,]
+			tmp = basename(site_days_cam_data$image_path)[1]
+            daydir =gsub(tmp,"", site_days_cam_data$image_path[1])
+			lstimgs =list.files(daydir,full.names=T,pattern = cam)
+###############			
+			
 
-  outdir= "E:\\SSL_DB_Tiles";dir.create(outdir)
-  # "C:\\Users\\usato\\SSL_DB\\TLC_UIA\\image_tiles.rds"
- # imgdata=read.csv( "C:\\Users\\usato\\SSL_DB\\TLC_markUp\\images_data.csv")
- # imgdata=imgdata[imgdata$site=="71"]
-   
-   site = gsub("site_","",imgdata$site)
-   RDSi=RDSdata[[imgdata$site]][[imgdata$poly]]
-
-  tile_sizes = RDSi[[1]]
- 
-  mask_x =  c(RDSi$Mask$x)
-  mask_y=  c(RDSi$Mask$y)
-  mask_x =c( mask_x , mask_x[1])
-  mask_y =c( mask_y , mask_y[1])
-  image_path <- imgdata$image_path
-   img <- readImage(image_path)
-   
+ ###############################################################
+# result=mclapply(lstimgs, function(lstimgs) {
+  for (imgs in 1:length(lstimgs)){
+    imgpth = lstimgs[imgs]
+   ##################################################################
+    img <- readImage(imgpth)
     img_dims = dim(img)
     img_width <- img_dims[1]
     img_height <- img_dims[2]
-   
-  bsnme= basename(image_path)
-  year=substr(bsnme,1,4)
-  day = substr(bsnme,1,8)
-  tilsDir=paste0(outdir,"\\",year,"_",site,"_Tiles");dir.create(tilsDir,showWarnings = F)
-  output_dir =paste0(tilsDir,"\\",day);dir.create(output_dir,showWarnings = F)
-##############################################################################################
+	################
+	##########################################################
+RDSi = RDSdata[[site]][[poly1]]
+			tile_sizes = RDSi[[1]]
+            mask_x =  c(RDSi$Mask$x)
+            mask_y=  c(RDSi$Mask$y)
+            mask_x =c( mask_x , mask_x[1])
+            mask_y =c( mask_y , mask_y[1])
+		
+           mask_polygon=NULL
+           mask_polygon <- st_polygon(list(cbind(mask_x, mask_y))) %>%
+           st_sfc() %>%
+           st_sf()
+         tile_sizes_rounded <- round(tile_sizes)
+ 
+ # Создаем кумулятивную сумму для получения Y-координат начала каждого ряда тайлов
+y_starts <- c(1, cumsum(tile_sizes_rounded)[-length(tile_sizes_rounded)] + 1)
+y_ends1 <- cumsum(tile_sizes_rounded)
+
+	# Корректируем последний тайл, чтобы он не выходил за границы изображения
+    y_ends <- pmin(y_ends1, img_height)
+	valid_rows <- which(y_starts <= img_height)
+	
+    bsnme= basename(imgpth)
+    year=substr(bsnme,1,4)
+    day <<- substr(bsnme,1,8)
+    tilsDir =paste0(outdir,"/",year,"_",site_no_site,"_Tiles");dir.create(tilsDir,showWarnings = F)
+   output_dir =paste0(tilsDir,"/",day);dir.create(output_dir,showWarnings = F)
+
+##########################################################
 # 4. Функция для проверки пересечения тайла с маской
 check_tile_corners <- function(x_start, y_start, tile_size, mask_polygon) {
   library(sf)
@@ -61,34 +167,15 @@ check_tile_corners <- function(x_start, y_start, tile_size, mask_polygon) {
   intersections <- st_intersects(corner_points, mask_polygon, sparse = FALSE)
   return(any(intersections))
 }
-###############################################################################################
-# Создаем объект полигона для маски используя sf
-mask_polygon=NULL
-mask_polygon <- st_polygon(list(cbind(mask_x, mask_y))) %>%
-  st_sfc() %>%
-  st_sf()
-  
- tile_sizes_rounded <- round(tile_sizes)
- 
- # Создаем кумулятивную сумму для получения Y-координат начала каждого ряда тайлов
-y_starts <- c(1, cumsum(tile_sizes_rounded)[-length(tile_sizes_rounded)] + 1)
-y_ends <- cumsum(tile_sizes_rounded)
- 
-# Корректируем последний тайл, чтобы он не выходил за границы изображения
-y_ends <- pmin(y_ends, img_height)
-valid_rows <- which(y_starts <= img_height)
-print(paste("Валидных рядов (в пределах изображения):", length(valid_rows), "\n"))
-
-
 # 6. Нарезка и сохранение тайлов
 ########################################################################
-for (i in 1:length(valid_rows)) {
+for (rows in 1:length(valid_rows)) {
   # Получаем размер тайла для текущего ряда
-  tile_size <- tile_sizes_rounded[i]
+  tile_size <- tile_sizes_rounded[rows]
   
   # Координаты Y для текущего ряда
-  y_start <- y_starts[i]
-  y_end <- y_ends[i]
+  y_start <- y_starts[rows]
+  y_end <- y_ends[rows]
   
   # Пропускаем если высота ряда меньше размера тайла
   if ((y_end - y_start + 1) < tile_size) next
@@ -96,24 +183,21 @@ for (i in 1:length(valid_rows)) {
   # Рассчитываем количество тайлов в текущем ряду по горизонтали
   n_tiles_x <- floor(img_width / tile_size)
     
-    print(paste("started row  ", i))
+   # print(paste("started row  ", rows))
     print(paste("number tiles in the row", n_tiles_x))
-  for (y in 1:n_tiles_x) {
+  for (tls in 1:n_tiles_x) {
     # Координаты X для текущего тайла
-    x_start <- (y - 1) * tile_size + 1
+    x_start <- (tls - 1) * tile_size + 1
     x_end <- x_start + tile_size - 1
     
     # Пропускаем если тайл выходит за границы по X
     if (x_end < img_width) {
     
-    # Проверяем пересечение с маской (используем быстрый метод по центру)
+    # Проверяем пересечение с маской
     if (check_tile_corners(x_start, y_start, tile_size, mask_polygon)==T) {
-  
-  
-       img_width
+      
+   	  img_width
        img_height
-  
-    
 		EBIy_start = img_height-y_start
 		EBIy_end = EBIy_start- tile_size + 1
 		
@@ -121,31 +205,36 @@ for (i in 1:length(valid_rows)) {
 		 
         # Вырезаем квадратный тайл
         tile <- img[x_start:x_end, EBIy_start:EBIy_end, ]
-		#tile=flop(tile)
 		tile=flip(tile)
         
   
 							
-			tile_name=paste0("row",i,
-			                "_col", y,
+			tile_name=paste0("row",rows,
+			                "_col", tls,
 							"_xstart_", x_start,
 							"_xend_", x_end,
 							"_ystart_", EBIy_start,
 							"_yend_",EBIy_end,
-							"#",basename(image_path))
+							"#",basename(imgpth))
 			
 							
         
         output_path <- file.path(output_dir, tile_name)
-        # Сохраняем тайл
         writeImage(tile, output_path, quality = 85)
+    }
+    }
+    }
+    }
+ 
+    #return(saved_count)
+	}
+#  }, mc.cores = num_cores)
+#################################################################  
+ }
+#  control_site_days_cam = data.frame(site=site,day=day1,cam=poly1)
+# control_tmp =rbind(control_site_days_cam,control_tmp)
+# write.csv(control_tmp,control_tmp_pth)
+ }
+ }
 
-    
-    }
-    }
-    }
-    }
-    }
-##############################################################
-    
 
